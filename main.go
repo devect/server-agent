38424c4a0f779e0d480b04ab2a778b823c988ef3
Marcos Aguayo <marcos@aguayo.es>
2019-01-08 00:55:42 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,64 +1,116 @@
 package main
 
 import (
-	"os"
-	"strconv"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"syscall"
 	"time"
 
-	"github.com/mackerelio/go-osstat/network"
-
-	"github.com/mackerelio/go-osstat/disk"
+	"github.com/mackerelio/go-osstat/cpu"
+	"github.com/mackerelio/go-osstat/memory"
+	"github.com/mackerelio/go-osstat/uptime"
 )
 
+type DiskStatus struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
 func main() {
-	f, _ := os.OpenFile("/usr/local/bin/drive.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	f2, _ := os.OpenFile("/usr/local/bin/network.txt", os.O_APPEND|os.O_WRONLY, 0644)
-
 	for true {
-		ds := getDisk()
-		nt := getNet()
-		currentTime := time.Now().String()
-		under := ""
-		for i := -3; i < len(currentTime); i++ {
-			under += "_"
+		loc, _ := time.LoadLocation("UTC")
+		start := time.Now().In(loc)
+		fmt.Println("Executing script", start)
+
+		getSystemData()
+
+		now := time.Now().In(loc)
+		sleep_time := 60 - now.Second()
+
+		if start.Minute() == now.Minute() {
+			time.Sleep(time.Duration(sleep_time) * time.Second)
 		}
-		under += "\n"
-		//println(under + "| " + currentTime + " |\n" + under + ds + "------------------------------------------------------------\n" + nt)
-
-		f.WriteString(under + "| " + currentTime + " |\n" + under + ds + "------------------------------------------------------------\n")
-		f2.WriteString(under + "| " + currentTime + " |\n" + under + nt + "------------------------------------------------------------\n")
-
-		time.Sleep(30 * time.Second)
-
 	}
 }
-func getDisk() string {
-	txt := ""
-	usage, _ := disk.Get()
-	totalRead := 0
-	totalWrite := 0
-	for i := 0; i < len(usage); i++ {
-		read := strconv.Itoa(int(usage[i].ReadsCompleted))
-		write := strconv.Itoa(int(usage[i].WritesCompleted))
-		txt += ("Drive : " + usage[i].Name + " | Reads : " + read + " | Writes: " + write + "\n")
-		totalRead += int(usage[i].ReadsCompleted)
-		totalWrite += int(usage[i].WritesCompleted)
+
+func getSystemData() {
+	jsonData := map[string]interface{}{
+		"disk_all":      nil,
+		"disk_used":     nil,
+		"disk_free":     nil,
+		"uptime":        nil,
+		"memory_total":  nil,
+		"memory_used":   nil,
+		"memory_cached": nil,
+		"memory_free":   nil,
+		"cpu_user":      nil,
+		"cpu_system":    nil,
+		"cpu_idle":      nil,
 	}
-	txt += ("Total Read : " + strconv.Itoa(totalRead) + " | Total Write : " + strconv.Itoa(totalWrite) + "\n")
-	return txt
+
+	before, err := cpu.Get()
+	if err != nil {
+		return
+	}
+
+	time.Sleep(time.Duration(40) * time.Second)
+
+	after, err := cpu.Get()
+	if err != nil {
+		return
+	}
+	total := float64(after.Total - before.Total)
+
+	// Disk
+	disk := DiskUsage("/")
+	jsonData["disk_all"] = float64(disk.All)
+	jsonData["disk_used"] = float64(disk.Used)
+	jsonData["disk_free"] = float64(disk.Free)
+
+	// Uptime
+	uptime, err := uptime.Get()
+	if err != nil {
+		return
+	}
+	jsonData["uptime"] = uptime
+
+	// Memory
+	memory, err := memory.Get()
+	if err != nil {
+		return
+	}
+	jsonData["memory_total"] = memory.Total
+	jsonData["memory_used"] = memory.Used
+	jsonData["memory_cached"] = memory.Cached
+	jsonData["memory_free"] = memory.Free
+
+	// CPU
+	jsonData["cpu_user"] = float64(after.User-before.User) / total * 100
+	jsonData["cpu_system"] = float64(after.System-before.System) / total * 100
+	jsonData["cpu_idle"] = float64(after.Idle-before.Idle) / total * 100
+
+	sendData(jsonData)
 }
-func getNet() string {
-	txt := ""
-	usage, _ := (network.Get())
-	totalDown := 0
-	totalUp := 0
-	for i := 0; i < len(usage); i++ {
-		Down := strconv.Itoa(int(usage[i].RxBytes))
-		Up := strconv.Itoa(int(usage[i].TxBytes))
-		txt += ("Network Device : " + usage[i].Name + " | Download : " + Down + " | Upload: " + Up + "\n")
-		totalDown += int(usage[i].RxBytes)
-		totalUp += int(usage[i].TxBytes)
+
+func sendData(jsonData map[string]interface{}) {
+	jsonValue, _ := json.Marshal(jsonData)
+	_, err := http.Post("URL_API_HERE", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
 	}
-	txt += ("Total Download : " + strconv.Itoa(totalDown) + " | Total Upload : " + strconv.Itoa(totalUp) + "\n")
-	return txt
+}
+
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	return
 }
